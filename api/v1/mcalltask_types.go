d76071ce4cc53e18d2e157e339ee12a2a6b16053
dhong <dhong@fortinet.com>
2025-10-10 09:18:10 -0700

    fix: Remove unused runtime import after DeepCopy removal

--- api/v1/mcalltask_types.go
+++ api/v1/mcalltask_types.go
@@ -3,7 +3,6 @@ package v1
 import (
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 )
 
 // McallTaskSpec defines the desired state of McallTask
